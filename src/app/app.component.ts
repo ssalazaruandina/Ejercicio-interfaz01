a6f6fe9f516013f2a83da36d5ef8e5b90e3c1f70
ssalazaruandina <019101931k@uandina.edu.pe>
2022-04-06 11:31:44 -0700

    interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Alumno, listaAlumnos } from './interfaces/alumno';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'interfaces-prueba01';
+  
+  public lista:Alumno[] = listaAlumnos;
+  public parcial1:number = 0;
+  public parcial2:number = 0;
+  public parcial3:number = 0;
+  public total: number = 0;
+  
+  onPromedio(): number{
+    this.total = 0;
+    this.total =(this.parcial1+this.parcial2+this.parcial3)/3
+    console.log(this.total);
+    return this.total;
+  }
+
+  //Guardar notas
+  public posicion: number = -1;
+  guardarPos(i: number){
+    this.total = 0;
+    this.posicion = i;
+    console.log(this.posicion);
+  }
+  
+  guardarNota(){
+    this.parcial1 = 0;
+    this.parcial2 = 0;
+    this.parcial3 = 0;
+    this.lista[this.posicion].nota=this.total;
+    console.log(this.lista[this.posicion]);
+  }
+
+  onConfirmar(){
+    alert("Se publicaron las notas");
+  }
 }
